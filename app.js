423c5fa14d99601ab57eb6e130e4cbb4e75bf3af
vikash-ag <vikash-ag@users.noreply.github.com>
2017-11-20 20:23:36 +0530

    first commit

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ let express = require('express'),
 
 app.set('port', process.env.PORT || 5000);
 
-//app.use('/', express.static(__dirname + '/www')); // serving company logos after successful authentication
+/*
+app.use('/', express.static(__dirname + '/www')); // serving company logos after successful authentication
 
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.post('/contact', contact.execute);
 app.post('/login', auth.loginLink);
 app.post('/logout', auth.logout);
 app.get('/login/:userId', auth.oauthLogin);
 app.get('/oauthcallback', auth.oauthCallback);
+*/
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
